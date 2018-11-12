043e5920ed75b897d19d3db27dd1e3b7351a3f24
A3a3e1 <pavel.voropaiev@elastic.io>
2018-11-12 18:30:18 +0200

    basic functionality and documentation added

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -1,34 +1,23 @@
 "use strict";
-const request = require('request-promise');
-
-module.exports = verify;
+const debug = require('debug')('Credentials');
 
 /**
- * Executes the verification logic by sending a simple to the Petstore API using the provided apiKey.
- * If the request succeeds, we can assume that the apiKey is valid. Otherwise it is not valid.
+ * This function will be called by the platform to verify credentials
  *
- * @param credentials object to retrieve apiKey from
- *
- * @returns Promise sending HTTP request and resolving its response
+ * @param credentials - input credentials
+ * @callback cb  - callback function
  */
-function verify(credentials) {
+module.exports = function verifyCredentials(credentials, cb) {
+  debug('Credentials configuration: %o', credentials);
 
-    // access the value of the apiKey field defined in credentials section of component.json
-    const apiKey = credentials.apiKey;
+  const configData = credentials.configData;
 
-    if (!apiKey) {
-        throw new Error('API key is missing');
-    }
-
-    // sending a request to the most simple endpoint of the target API
-    const requestOptions = {
-        uri: 'https://petstore.elastic.io/v2/user/me',
-        headers: {
-            'api-key': apiKey
-        },
-        json: true
-    };
-
-    // if the request succeeds, we can assume the api key is valid
-    return request.get(requestOptions);
-}
+  try {
+    JSON.parse(configData);
+    return cb(null, {verified: true});
+  }
+  catch (err) {
+    console.error('Error occurred. Input object must be a valid JSON.');
+    return cb(null, {verified: false});
+  }
+};
